39abe27409b34f7e98183904f491c314acd41fb1
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2022-01-23 12:54:03 +0400

    Bug fix

--- coprime.c
+++ coprime.c
@@ -19,7 +19,7 @@ int main() {
 
 int coprime(int num1, int num2) {
     int min = num1 < num2 ? num1 : num2;
-    for (int i = 2; i < min; i++)
+    for (int i = 2; i <= min; i++)
         if (num1 % i == 0 && num2 % i == 0)
             return 0;
     
