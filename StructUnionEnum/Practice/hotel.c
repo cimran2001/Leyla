de31f1e33ab45bcb30e37e7b0d186e61e34e7a53
c.imran2001 <c.imran2001@yandex.com>
2022-02-20 12:58:52 +0400

    Homework added

--- StructUnionEnum/Practice/hotel.c
+++ StructUnionEnum/Practice/hotel.c
@@ -10,8 +10,8 @@ typedef struct {
 
 typedef struct {
     const char *name;
-    Apartment apartments[MAX_APARTMENTS_COUNT];
-    size_t ordered;
+    Apartment apartments[MAX_APARTMENTS_COUNT]; // 0 1 2 3 4 5 6 7
+    size_t ordered; // 4
 } Hotel;
 
 Hotel createHotel(const char *name);
@@ -50,8 +50,8 @@ Hotel createHotel(const char *name) {
 
     srand(time(NULL));
     for (size_t i = 0; i < MAX_APARTMENTS_COUNT; i++) {
-        size_t roomsCount = rand() % 5 + 1;
-        double price = (rand() % 90000 + 10000) / 100.;
+        size_t roomsCount = rand() % 5 + 1;             // 1 - 5
+        double price = (rand() % 90000 + 10000) / 100.; // 100.00 - 999.99
         hotel.apartments[i] = createApartment(roomsCount, price);
     }
 
@@ -79,7 +79,9 @@ Apartment orderApartment(Hotel *hotel) {
     if (hotel->ordered == MAX_APARTMENTS_COUNT)
         return createApartment(0, 0.);
 
-    Apartment result = hotel->apartments[hotel->ordered];
-    hotel->ordered++;
-    return result;
+    // Apartment result = hotel->apartments[hotel->ordered];
+    // hotel->ordered++;
+    // return result;
+    // OR
+    return hotel->apartments[hotel->ordered++];
 }
